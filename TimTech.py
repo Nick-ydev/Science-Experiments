a9e4a01d71c009df33971f7fe361d1066eb344b0
Nikhil <nikhil.yadav.git.hub@gmail.com>
2024-01-17 21:33:22 +0530

    Record no. of Wins

--- TimTech.py
+++ TimTech.py
@@ -1,28 +1,31 @@
 import random
 
 token = ["R","P","S"]
+user_wins = 0
+computer_wins = 0
 
-computer_guess = token[random.randrange(3)]
-print(computer_guess)
-user_guess = input("Type one from Rock Paper Scissor R/P/S! ")
-
+no_of_games = 0
 draw_index = 0
-while user_guess == computer_guess:
-    print("Match Draw")
+
+while no_of_games < 6:
     computer_guess = token[random.randrange(3)]
     print(computer_guess)
     user_guess = input("Type one from Rock Paper Scissor R/P/S! ")
-    draw_index += 1
-    
+    if user_guess == "R" and computer_guess == "P":
+        print("Computer selected",computer_guess, "CPU wins")
+        computer_wins += 1
+    elif user_guess == "P" and computer_guess == "S":
+        print("Computer selected",computer_guess, "CPU wins")
+        computer_wins += 1
+    elif user_guess == "S" and computer_guess == "R":
+        print("Computer selected",computer_guess, "CPU wins")
+        computer_wins += 1
+    elif user_guess == computer_guess:
+        draw_index += 1
+        print ("Match Draw")
+    else:
+        print("Congratzi You WIN")
+        user_wins += 1
+    no_of_games += 1
 
-if user_guess == "R" and computer_guess == "P":
-    print("Computer selected",computer_guess, "CPU wins")
-elif user_guess == "P" and computer_guess == "S":
-    print("Computer selected",computer_guess, "CPU wins")
-elif user_guess == "S" and computer_guess == "R":
-    print("Computer selected",computer_guess, "CPU wins")
-else:
-    print("Congratzi Nazi")
-
-
-print("No. of rematches", draw_index)
+print("No. of matches",no_of_games,"Draws:", draw_index,"Computer wins:", computer_wins, "User wins:" ,user_wins)
