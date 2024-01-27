34a6bcae3dcc94637be362e89208a71a79916c77
Nikhil <nikhil.yadav.git.hub@gmail.com>
2024-01-27 11:58:27 +0530

    Bagels

--- Bagels.py
+++ Bagels.py
@@ -1,17 +1,31 @@
 import random
+no_of_digits = input("For how many digits(atleast) would you like to play?")
 
-computer_guess = str(369)
+upper_bound = 9*no_of_digits
+print(upper_bound)
+computer_guess = str(random.randrange(100,1000))
 
 #str(random.randrange(100,1000))
 
+# print(computer_guess)
+no_of_guesses = 0
+i=0 
+while no_of_guesses < 11:
+    user_guess = str(input("Guess the number?"))
+    if user_guess == computer_guess:
+        print ("You Won")
+        break
+    for i in range(0,3):
+        if user_guess[i] == computer_guess[i]:
+            print("Fermi")
+        elif user_guess[i] in computer_guess:
+            print("Pico")
+        else:
+            print("Bagels")
+        
+    no_of_guesses += 1
+
 print(computer_guess)
-
-user_guess = str(432)
-
-for i in range(0,3):
-    if user_guess[i] == computer_guess[i]:
-        print("Fermi")
-    else:
-        user_guess[i] in computer_guess[i]
-        print("Pico")
+    
+        
 
