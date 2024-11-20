ad41352fbd4080d88f90926c3bbc40bce421404e
Nikhil <nikhil.yadav.git.hub@gmail.com>
2024-11-20 22:40:40 +0530

    Pndas Experiment

--- Collatz Conjecture.py
+++ Collatz Conjecture.py
@@ -1,19 +1,31 @@
 import random
 
-number = random.randrange(1000,100000)
-print("The Number is" , number)
-remainder = number % 2
+lower_bound = 3
+upper_bound = 6
+final_iteration = 0
+    
 
-iterations = 0
-while number > 1:
-    if (number % 2) == 0:
-        number /= 2
-        print(number)
-    elif (number % 2) == 1:
-        number = (3*number) + 1
-        print(number)
-    iterations += 1
+def collatz_sequence (number,start_iterations):
+    while number > 1:
+        if (number % 2) == 0:
+            number /= 2
+            print(number)
+        elif (number % 2) == 1:
+            number = (3*number) + 1
+            print(number)
+        start_iterations += 1
+    print("No. of Iterations =", start_iterations)
+    if start_iterations > final_iteration:
+        final_iteration = start_iterations
+    
+    print("Maxima of Iterations=",final_iteration)
+    return final_iteration
 
-print("No. of Iterations =", iterations)
+for i in range(lower_bound, upper_bound):
+    number = i #random.randrange(1000,100000)
+    print("The Number is" , number)
+    start_iterations = 0
+    print(collatz_sequence(number,start_iterations))
+
 
 
--- Pandas experiments.py
+++ Pandas experiments.py
@@ -2,32 +2,46 @@ import pandas as pd
 import random
 import numpy as np
 
-number = random.randrange(1,100)
-print("The Number is" , number)
-remainder = number % 2
+lower_bound = 3
+upper_bound = 6
 
-iteration_list = np.array([0])
-number_track = np.array([number])
+iteration_list = [0]
 
-iterations = 0
-while number > 1:
-    if (number % 2) == 0:
-        number /= 2
-        print(number)
-    elif (number % 2) == 1:
-        number = (3*number) + 1
-        print(number)
-    iterations += 1
-    iteration_list.append(iterations)
-    number_track.append(number)
+start_iterations = 0
+final_iterations = 0
 
-print("No. of Iterations =", iterations)
+for i in range(lower_bound,upper_bound):
+    number = i
+    number_track = [number]
+    while number > 1:
+        if (number % 2) == 0:
+            number /= 2
+            print(number)
+        elif (number % 2) == 1:
+            number = (3*number) + 1
+            print(number)
+        start_iterations += 1
+        if start_iterations > final_iterations:
+            final_iterations = start_iterations
+        number_track.append(number)
+
+#Append no. of iteration in a list for numpy to execute
+for i in range(0,final_iterations):
+    iteration_list.append(i)
+    print("The Number is" , number)
+
+iteration_list_numpy = np.array(iteration_list)
+number_track_numpy = np.array(number_track)
+
+print("No. of Iterations =", start_iterations)
 data = {
-  "iterations": iteration_list,
-  "number": number_track
+  "iterations": iteration_list_numpy,
+  "number": number_track_numpy
 }
 
 #load data into a DataFrame object:
-df = pd.DataFrame(data)
+df = pd.DataFrame(data, index= (iteration_list_numpy))
 
+# saving the dataframe and exporting in CSV
+#df.to_csv(r'D:\Power BI thing\file3.csv')
 print(df) 
