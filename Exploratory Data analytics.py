f170fb89a66f203c9fe5e0d1177434037ee99a06
Nikhil <nikhil.yadav.git.hub@gmail.com>
2025-03-16 22:28:08 +0530

    New

--- Exploratory Data analytics.py
+++ Exploratory Data analytics.py
@@ -6,3 +6,15 @@ import seaborn as sns
 happiness_data = pd.read_csv('D:\DATA SCIENCE Experiments\Happiness Index.csv')
 
 print(happiness_data.head())
+#print(happiness_data.describe())
+#print(happiness_data.isnull().sum())
+
+duplicatesValues = happiness_data.duplicated()
+print(duplicatesValues.sum())
+happiness_data[duplicatesValues]
+
+from sklearn.preprocessing import StandardScaler
+stdScale = StandardScaler()
+stdScale
+happiness_data['Happiness Score'] = stdScale.fit_transform(happiness_data[['Happiness Score']])
+happiness_data.head()
