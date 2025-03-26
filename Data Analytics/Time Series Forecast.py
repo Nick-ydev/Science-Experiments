84b6e38d4b09709e3c81153ca4c92f8f97ee9cf3
Nikhil <nikhil.yadav.git.hub@gmail.com>
2025-03-26 22:24:31 +0530

    Redos

--- Data Analytics/Time Series Forecast.py
+++ Data Analytics/Time Series Forecast.py
@@ -22,6 +22,29 @@ plt.show()
 
 from statsmodels.tsa.stattools import adfuller
 
+#check Stationarity
 result = adfuller(data['Dates']) 
 print('ADF Statistic:', result[0])
 print('p-value:', result[1])
+
+from statsmodels.tsa.arima.model import ARIMA
+# ARIMA model parameters (p, d, q)
+p = 5  # AutoRegressive (AR) order
+d = 1  # Differencing order
+q = 0  # Moving Average (MA) order
+
+model = ARIMA(data, order=(p, d, q))
+model_fit = model.fit()
+
+forecast_steps = 10
+forecast = model_fit.forecast(steps=forecast_steps)
+print(forecast)
+
+plt.plot(data, label='Original Data')
+plt.plot(range(len(data), len(data) + forecast_steps), forecast, color='red', label='Forecast')
+plt.title('Time Series Forecast')
+plt.xlabel('Time')
+plt.ylabel('Value')
+plt.legend()
+plt.show()
+
