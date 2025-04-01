bee5872b311f8a093598d4a9e8d030618330cbdf
Nikhil <nikhil.yadav.git.hub@gmail.com>
2025-04-01 23:19:09 +0530

    JPMC expertiments

--- Data Analytics/NachiketARIMA.py
+++ Data Analytics/NachiketARIMA.py
@@ -3,10 +3,18 @@ import pandas as pd
 from matplotlib import pyplot
 from statsmodels.tsa.ar_model import AutoReg
 
-df = pd.read_csv('D:\Power BI thing\Forage Data sets\JPMC Data analyst\At_Gas.csv',index_col=0,parse_dates=True)
-X = df.values
-print('Shape of Data \t',df.shape)
-print('original Dataset:\n',df.head)
-print('After extraxting: \n', X)
+# df = pd.read_csv('D:\Power BI thing\Forage Data sets\JPMC Data analyst\At_Gas.csv',index_col=0,parse_dates=True)
+# X = df.values
+# print('Shape of Data \t',df.shape)
+# print('original Dataset:\n',df.head)
+# print('After extraxting: \n', X)
 
-df.plot
+# df.plot
+
+for month in range(0,13):
+    print (month, "bf4")
+    month = ((month + 1) % 12) or 12 # 1 or 12 > 1
+    print (month, "after")
+    if month == 1:
+        month =+ 1
+    
--- Jupyter Notebook/Forecast.md
+++ Jupyter Notebook/Forecast.md
@@ -1,1 +1,4 @@
+Experimenting with various [p,d,q]
+
+(2,1,1)
 ![alt text](image.png)
